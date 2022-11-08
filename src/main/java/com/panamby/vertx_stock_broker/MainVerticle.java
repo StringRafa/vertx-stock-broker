9c750e7d5dd02c139ba74efc78b1858b3901dbb5
Rafael <rafael.souza@prill.com.br>
2022-11-08 11:51:39 -0300

    Adicionando log

--- src/main/java/com/panamby/vertx_stock_broker/MainVerticle.java
+++ src/main/java/com/panamby/vertx_stock_broker/MainVerticle.java
@@ -2,9 +2,26 @@ package com.panamby.vertx_stock_broker;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MainVerticle extends AbstractVerticle {
 
+  public static void main(String[] args) {
+	  var vertx = Vertx.vertx();
+	  vertx.exceptionHandler( error -> 
+		 log.error("Unhandled: {}", error)
+	  );
+	  vertx.deployVerticle(new MainVerticle(), ar -> {
+		  if(ar.failed()) {
+			  log.error("Failed to deploy: {}", ar.cause());
+			  return;
+		  }
+		  log.info("Deployed {}!", MainVerticle.class.getName());
+	  });
+}
+	
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
